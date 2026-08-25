846db7985809d6573cc663bb28c406b11aeff436
Valentina Ceban <cebanvalentina9@gmail.com>
2026-08-25 18:29:40 +0300

    Initial commit

---
+++ src/main/java/pixel_academy/primul_exercitiu/PrimulExercitiuApplication.java
@@ -0,0 +1,13 @@
+package pixel_academy.primul_exercitiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimulExercitiuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimulExercitiuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pixel_academy/primul_exercitiu/PrimulExercitiuApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel_academy.primul_exercitiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimulExercitiuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
